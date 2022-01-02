f14721a3db9a649785b36e78a9ca194ff0e59eef
bulutatay <bulutatay@hotmail.com>
2022-01-02 18:57:36 +0300

    Initial commit

---
+++ src/Java101.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Java101 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String ad;
+        String soyad;
+
+        System.out.print("Adınız: ");
+        ad = input.nextLine();
+
+        System.out.print("Soyadınız: ");
+        soyad = input.nextLine();
+
+        System.out.print(ad + " " + soyad);
+    }
+}
---
+++ src/NotOrtalamasi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        System.out.print("Matematik Notunuzu Giriniz: ");
+        mat = input.nextInt();
+        System.out.print("Fizik Notunuzu Giriniz: ");
+        fizik = input.nextInt();
+        System.out.print("Kimya Notunuzu Giriniz: ");
+        kimya = input.nextInt();
+        System.out.print("Türkçe Notunuzu Giriniz: ");
+        turkce = input.nextInt();
+        System.out.print("Tarih Notunuzu Giriniz: ");
+        tarih = input.nextInt();
+        System.out.print("Müzik Notunuzu Giriniz: ");
+        muzik = input.nextInt();
+
+        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
+        double sonuc = toplam / 6;
+        String durum = sonuc >= 60 ? "Geçtiniz" : "Geçemediniz";
+
+        System.out.println("Not Ortalamanız: " + sonuc);
+        System.out.print("Sınıfı " + durum);
+
+    }
+}
