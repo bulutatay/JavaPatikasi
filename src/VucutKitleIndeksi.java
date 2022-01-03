4699440a19362e63f578eeef9d32ec943a5f535b
bulutatay <bulutatay@hotmail.com>
2022-01-03 08:45:56 +0300

    Vücut Kitle İndeksi

---
+++ src/VucutKitleIndeksi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double boy, kilo, indeks;
+
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kilo = input.nextDouble();
+
+        indeks = kilo / (boy * boy);
+
+        System.out.print("Vücut Kitle İndeksiniz: " + indeks);
+    }
+}
