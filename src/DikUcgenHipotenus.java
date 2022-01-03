6e8ce21b01e1bb249c21911a6a3c3ab7304e48b3
bulutatay <bulutatay@hotmail.com>
2022-01-03 07:52:36 +0300

    DikUcgenHipotenus

---
+++ src/DikUcgenHipotenus.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DikUcgenHipotenus {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double a, b, c, cevre, alan, islem;
+
+        System.out.print("Üçgenin 1. Kenar Uzunluğu: ");
+        a = input.nextDouble();
+
+        System.out.print("Üçgenin 2. Kenar Uzunluğu: ");
+        b = input.nextDouble();
+
+        System.out.print("Üçgenin 3. Kenar Uzunluğu: ");
+        c = input.nextDouble();
+
+        islem = (a+b+c)/2;
+        cevre = 2*islem;
+        alan = Math.sqrt(islem * (islem - a) * (islem - b) * (islem - c));
+
+        System.out.println("Üçgenin Çevresi: " + cevre);
+        System.out.println("Üçgenin Alanı: " + alan);
+    }
+}
