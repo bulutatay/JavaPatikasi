565c30953972218eb95086071ffe26c46e2f46ee
bulutatay <bulutatay@hotmail.com>
2022-01-03 08:32:10 +0300

    Daire Alan-Çevre

---
+++ src/DaireAlanCevre.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DaireAlanCevre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double r, a, alan, cevre, pi = 3.14, dilim;
+
+        System.out.print("Dairenin yarı çapını giriniz: ");
+        r = input.nextDouble();
+
+        System.out.print("Dairenin merkez açı ölçüsünü giriniz: ");
+        a = input.nextDouble();
+
+        alan = pi * r * r;
+        cevre = 2 * pi * r;
+        dilim = (pi * (r*r) * a) / 360;
+
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Çevresi: " + cevre);
+        System.out.println("Daire Diliminin Alanı: " + dilim);
+    }
+}
---
+++ src/TaksiMetre.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TaksiMetre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double mesafe, ucret, sonuc;
+        System.out.print("Taksi ile gidilen mesafeyi giriniz (km): ");
+        mesafe = input.nextDouble();
+
+        ucret = 10 + (mesafe*2.20);
+        sonuc = ucret < 20 ? 20 : ucret;
+
+        System.out.print("Taksi Ücretiniz: " + sonuc + " TL 'dir.");
+    }
+}
