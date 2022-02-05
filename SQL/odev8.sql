51508795a67c82dc6753179a1728fe7d68b91841
Bulut ATAY <51698796+bulutatay@users.noreply.github.com>
2022-02-05 15:30:24 +0300

    odev8

---
+++ SQL/odev8.sql
@@ -0,0 +1,14 @@
+--CREATE TABLE employee (
+--	id SERIAL PRIMARY KEY, 
+--	name VARCHAR(50) NOT NULL, 
+--	birthday DATE, 
+--	email VARCHAR(100)
+--);
+
+--UPDATE employee SET name = 'Test' WHERE id = 1;
+--UPDATE employee SET birthday = '2022.02.05' WHERE id = 2;
+
+--DELETE FROM employee WHERE name = 'Nicol';
+--DELETE FROM employee WHERE id = 7;
+
+--SELECT * FROM employee;
