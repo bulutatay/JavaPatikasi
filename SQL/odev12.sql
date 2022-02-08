1ccc8a001daad6de07b17b19b0677a7029b80a71
Bulut ATAY <51698796+bulutatay@users.noreply.github.com>
2022-02-08 20:46:40 +0300

    odev12

---
+++ SQL/odev12.sql
@@ -0,0 +1,4 @@
+-- SELECT COUNT(*) FROM Film WHERE length > (SELECT AVG(length) FROM Film)
+-- SELECT COUNT(*) FROM Film WHERE rental_rate = any (SELECT max(rental_rate) FROM Film);
+-- SELECT COUNT(*) FROM Film WHERE rental_rate = all (SELECT min(rental_rate) FROM Film) and replacement_cost = any (SELECT min(replacement_cost) FROM Film)
+-- SELECT customer_id, count(payment_id) "Alışveriş Toplamı" FROM payment GROUP BY customer_id ORDER BY "Alışveriş Toplamı" DESC
